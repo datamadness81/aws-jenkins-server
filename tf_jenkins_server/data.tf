7728ccdb1142a29c0335c0698fc5e3b9fcfe179b
John Ramirez <jhon.ramirez@globant.com>
2022-01-09 21:59:54 -0500

    update playbook1

--- tf_jenkins_server/data.tf
+++ tf_jenkins_server/data.tf
@@ -6,7 +6,7 @@ data "aws_ami" "jenkins_server" {
   most_recent = true
   owners      = ["amazon"]
   filter {
-    image_id   = "ImageId"
+    image_id   = "id"
     values = ["ami-04505e74c0741db8d"]
   }
 }
