0771e57abe0164fe2acacf126d2facc9bd835aa7
John Ramirez <jhon.ramirez@globant.com>
2022-01-09 22:29:25 -0500

    update playbook1

--- tf_jenkins_server/data.tf
+++ tf_jenkins_server/data.tf
@@ -2,19 +2,19 @@
 #                   DATA SOURCE
 #----------------------------------------------------
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "jenkins_server" {
+  most_recent = true
 
-    most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
+  owners = ["099720109477"]
 
-    owners = ["099720109477"]
 }
